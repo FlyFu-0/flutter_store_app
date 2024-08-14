a8435c5a9bd0ff610069a7ba4b15cc9047efea1b
FlyFu-0 <tihonov-04@yandex.ru>
2024-08-14 20:07:28 +0300

    categories on home screen

--- lib/features/shop/home/home.dart
+++ lib/features/shop/home/home.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
 import '../../../common/widgets/custom_shpaes/containers/primary_header_container.dart';
 import '../../../common/widgets/custom_shpaes/containers/search_container.dart';
+import '../../../common/widgets/image_text_widgets/vertical_image_text.dart';
+import '../../../common/widgets/texts/section_heading.dart';
 import 'widgets/home_appbar.dart';
+import 'widgets/home_categories.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -21,7 +26,26 @@ class HomeScreen extends StatelessWidget {
                 SizedBox(
                   height: TSizes.spaceBtwSections,
                 ),
-                TSearchContainer(text: 'Search in Store')
+                TSearchContainer(text: 'Search in Store'),
+                SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                  child: Column(
+                    children: [
+                      TSectionHeading(
+                        title: 'Popular Categories',
+                        showActionButton: false,
+                        textColor: TColors.white,
+                      ),
+                      SizedBox(
+                        height: TSizes.spaceBtwItems,
+                      ),
+                      THomeCategories()
+                    ],
+                  ),
+                )
               ],
             ))
           ],

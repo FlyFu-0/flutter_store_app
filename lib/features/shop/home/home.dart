f45c14587a4e47da128397002de8b9934161760b
FlyFu-0 <tihonov-04@yandex.ru>
2024-08-14 18:49:22 +0300

    search container

--- lib/features/shop/home/home.dart
+++ lib/features/shop/home/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:t_store/utils/constants/sizes.dart';
 
 import '../../../common/widgets/custom_shpaes/containers/primary_header_container.dart';
+import '../../../common/widgets/custom_shpaes/containers/search_container.dart';
 import 'widgets/home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,12 +15,15 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: Column(
-                children: [
-                  THomeAppBar()
-                ],
-              )
-              )
+                child: Column(
+              children: [
+                THomeAppBar(),
+                SizedBox(
+                  height: TSizes.spaceBtwSections,
+                ),
+                TSearchContainer(text: 'Search in Store')
+              ],
+            ))
           ],
         ),
       ),

a2f3cc08291197b03aaa379899ff6cc5e46a15cc
FlyFu-0 <tihonov-04@yandex.ru>
2024-08-19 19:25:13 +0300

    cart screen

---
+++ lib/features/shop/screens/cart/cart.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/texts/product_price_text.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+
+import '../../../../common/widgets/products/cart/cart_item.dart';
+import '../../../../common/widgets/products/cart/product_quantity_with_add_and_remove_button.dart';
+
+class CartScreen extends StatelessWidget {
+  const CartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppBar(
+        showBackArrow: true,
+        title: Text(
+          'Cart',
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: ListView.separated(
+            separatorBuilder: (_, __) => const SizedBox(
+              height: TSizes.spaceBtwSections,
+            ),
+            itemCount: 10,
+            shrinkWrap: true,
+            itemBuilder: (_, index) => const Column(
+              children: [
+                TCartItem(),
+                SizedBox(
+                  height: TSizes.spaceBtwItems,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(width: 70),
+                        TProductQuantityWithAddAndRemoveButton(),
+                      ],
+                    ),
+                    TProductPriceText(price: '256'),
+                  ],
+                ),
+              ],
+            ),
+          )),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ElevatedButton(
+            onPressed: () {}, child: const Text('Checkout \$256')),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home/widgets/home_appbar.dart
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:t_store/features/shop/screens/cart/cart.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
 
@@ -20,7 +22,7 @@ class THomeAppBar extends StatelessWidget {
         ],
       ),
       actions: [
-        TCartCounterIcon(onPressed: () {}, iconColor: TColors.white, notifCircleColor: TColors.black,),],
+        TCartCounterIcon(onPressed: () => Get.to(() => const CartScreen()), iconColor: TColors.white, notifCircleColor: TColors.black,),],
     );
   }
 }
